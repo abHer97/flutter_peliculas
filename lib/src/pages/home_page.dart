36f391ae72891dc0eb317b6d223bfacc9cd47bc6
Abel Herrera <ahpard97@gmail.com>
2021-06-07 12:54:37 -0500

    Agregue un comentario en main para desactivar el safety null y poder usar la libreria de swiper. Agregue el swiper como dependencia del proyecto y lo llame en homePage para muestra

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Text('Hola mundo')));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Peliculas en cines'),
+          centerTitle: false,
+          backgroundColor: Colors.indigoAccent,
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+        ),
+        body: Container(
+          child: Column(
+            children: [_swiperTarjetas()],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      padding: EdgeInsets.only(top: 10.0),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        // pagination: SwiperPagination(),
+        // control: SwiperControl(),
+      ),
+    );
   }
 }
